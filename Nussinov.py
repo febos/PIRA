a28a54574e9174550c4fcab8e17d79b4730b3e86
Eugene Baulin <ebaulin@iimcb.gov.pl>
2023-12-12 15:26:16 +0100

    Nussinov started

--- Nussinov.py
+++ Nussinov.py
@@ -1,17 +1,53 @@
 import numpy as np
 
-def Nussinov(seq):
+def Nussinov(seq, minloop = 3):
 
     seq = seq.upper()
+    N   = len(seq)
 
     bps = {"GU":1,"UG":1,
            "AU":2,"UA":2,
            "GC":3,"CG":3}
 
+    D = np.zeros((N,N))
+    K = {}
+
+    for h in range(N):
+        for i in range(N-h):
+
+            j = i+h
+
+            if j - i - 1 < minloop:
+
+                D[i,j] = 0
+
+            else:
+
+                bestk, bestscorek = -1, -1
+
+                for k in range(i, j-minloop):
+
+                    if seq[k] + seq[j] in bps:
+                        scorek = D[i, k - 1] + D[k + 1, j - 1] + bps[seq[k] + seq[j]]
+                        if scorek > bestscorek:
+                            bestk, bestscorek = k, scorek
+
+                if bestscorek >= D[i, j - 1]:
+
+                    K[(i, j)] = bestk
+                    D[i, j]   = bestscorek
+
+                else:
+                    D[i, j] = D[i, j - 1]
+
+    print(D)
+    print(K)
+                            
 
 if __name__ == "__main__":
 
-    seq = "ACGUACGCUAGCUGCUCGAUCGUCGAUCGAUCGACGCUAGCGCGUCGGGU"
+    #seq = "ACGUACGCUAGCUGCUCGAUCGUCGAUCGAUCGACGCUAGCGCGUCGGGU"
+    seq = "GGUCCAC"
 
     dbn = Nussinov(seq)
 
--- README.md
+++ README.md
@@ -4,6 +4,29 @@ Python implementations of RNA algorithms
 
 ## References
 
-https://rna.informatik.uni-freiburg.de/Teaching/
+https://rna.informatik.uni-freiburg.de/Teaching/ (https://doi.org/10.1093/nar/gky329)
 
 
+## TODO
+
+implement Nussinov
+
+read Nussinov1978
+
+read Wuchty1999
+
+implement Wuchty
+
+_____________________________________
+
+Kabshch algorithm
+
+Turner algorithm
+
+McCaskill algorithm
+
+Sankoff algorithm
+
+SCFG
+
+Furier transformations
