badf3c48183ba66cc3217ca213b47be725a57caf
Eugene Baulin <ebaulin@iimcb.gov.pl>
2024-02-06 11:41:17 +0100

    finished Nussinov.py

--- Nussinov.py
+++ Nussinov.py
@@ -2,15 +2,55 @@ import numpy as np
 
 ### https://doi.org/10.1073/pnas.77.11.6309
 
+# base pair scores
+bps = {"GU":-1,"UG":-1,
+       "AU":-2,"UA":-2,
+       "GC":-3,"CG":-3}
+
+
+def BackTrack(begin, end, K, minloop, partial = False):
+
+    queue = {(begin, end), } # start from the last cell
+    basepairs = []
+
+    while queue:
+
+        newq = set()
+
+        # for each cell in the queue
+        for i,j in queue:
+            # if we have (k, j) base pair
+            if (i,j) in K:
+                k = K[(i,j)]
+                # if we may have more base pairs before k  
+                if (k - 1) - i > minloop:
+                    newq.add((i, k - 1))
+                # if we may have more base pairs inside [k, j]
+                if (j - 1) - (k + 1) > minloop and not partial:
+                    newq.add((k + 1, j - 1))
+                # add (k, j) bp
+                basepairs.append((k,j))
+            # if j is unpaired
+            else:
+                # if we may have more base pairs inside [i, j - 1]
+                if (j - 1) - i > minloop:
+                    newq.add((i, j - 1))
+        # update queue
+        queue = newq
+
+    return sorted(basepairs)
+
+
+def Ekj(seq,k,j,inners):
+
+    bpstep = len(inners) == 1 and inners[0][0] == k + 1 and inners[0][1] == j - 1
+    return bps[seq[k]+seq[j]]
+
+
 def Nussinov(seq, minloop = 3):
 
     seq = seq.upper()
     N   = len(seq)    # sequence length
-
-    # base pair scores
-    bps = {"GU":1,"UG":1,
-           "AU":2,"UA":2,
-           "GC":3,"CG":3}
 
     # score matrix
     D = np.zeros((N,N))
@@ -26,16 +66,17 @@ def Nussinov(seq, minloop = 3):
             # column index
             j = i + h
 
-            bestk, bestscorek = -1, -1
+            bestk, bestscorek = -1, 10**9
             # searching for the optimal base pair
             for k in range(i, j - minloop):
 
                 if seq[k] + seq[j] in bps:
-                    scorek = D[i, k - 1] + D[k + 1, j - 1] + bps[seq[k] + seq[j]]
-                    if scorek > bestscorek:
+                    inners = BackTrack(k + 1, j - 1, K, minloop, partial = True)
+                    scorek = D[i, k - 1] + D[k + 1, j - 1] + Ekj(seq,k,j,inners)
+                    if scorek < bestscorek:
                         bestk, bestscorek = k, scorek
             # see if no base pair is not better
-            if bestscorek >= D[i, j - 1]:
+            if bestscorek <= D[i, j - 1]:
 
                 K[(i, j)] = bestk
                 D[i, j]   = bestscorek
@@ -45,44 +86,38 @@ def Nussinov(seq, minloop = 3):
 
     # traceback
     dbn = ['.' for ch in seq] # dbn template
-
-    queue = {(0, N - 1), } # start from the last cell
-
-    while queue:
-
-        newq = set()
-
-        # for each cell in the queue
-        for i,j in queue:
-            # if we have (k, j) base pair
-            if (i,j) in K:
-                k = K[(i,j)]
-                # if we have more base pairs before k  
-                if D[(i, k - 1)] > 0:
-                    newq.add((i, k - 1))
-                # if we have more base pairs inside [k, j]
-                if D[(k + 1, j - 1)] > 0:
-                    newq.add((k + 1, j - 1))
-                # add (k, j) bp to dbn
-                dbn[k] = '('
-                dbn[j] = ')'
-            # if j is unpaired
-            else:
-                # if we have more base pairs inside [i, j - 1]
-                if D[(i, j - 1)] > 0:
-                    newq.add((i, j - 1))
-        # update queue
-        queue = newq
+    predictedbps = BackTrack(0, N - 1, K, minloop)
+    for v,w in predictedbps:
+        dbn[v] = '('
+        dbn[w] = ')'
     
-    return ''.join(dbn)
+    return D[0, N - 1], ''.join(dbn)
                             
 
 if __name__ == "__main__":
 
     seq = "ACGUACGCUAGCUGCUCGAUCGUCGAUCGAUCGACGCUAGCGCGUCGGGU"
+    '''
+    ACGUACGCUAGCUGCUCGAUCGUCGAUCGAUCGACGCUAGCGCGUCGGGU
+    (((.((((..((((((((((((.....))))))).)).))))))))..))
+    51.0
+    '''
+
+    seq = 'GGGCGGCUAGCUCAGCGGAAGAGCGCUCGCCUCACACGCGAGAGGUCGUAGGUUCAAGUCCUACGCCGCCCACCA'
+    '''
+    GGGCGGCUAGCUCAGCGGAAGAGCGCUCGCCUCACACGCGAGAGGUCGUAGGUUCAAGUCCUACGCCGCCCACCA
+    (((((((..((((....[..)))).(((((.......))))).....(((((..]....))))))))))))....
+
+    GGGCGGCUAGCUCAGCGGAAGAGCGCUCGCCUCACACGCGAGAGGUCGUAGGUUCAAGUCCUACGCCGCCCACCA
+    (((.((((..(((.((((..(((.(....)))).).)))))))((.(((((((...)..)))))))))))).)).
+    ((((((((.((((.......)))).(((((.(...).)))))))..(((((((...)..))))))))))))....
+    75.0
+    '''
+    
     #seq = "GGUUCCAC"
 
-    dbn = Nussinov(seq)
+    score, dbn = Nussinov(seq)
 
     print(seq)
     print(dbn)
+    print(score)
